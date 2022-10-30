dcc2027c14cde92e18a6a6259d11a748b1e2243a
AshaniImalsha <ashaniimalsha26@gmail.com>
2022-10-30 21:30:34 +0530

    change font style

--- boardimafrontend/src/App.js
+++ boardimafrontend/src/App.js
@@ -3,26 +3,39 @@ import LandingMain from './components/landing/LandingMain';
 import SignIn from './components/login/SignIn';
 import SignUp from './components/login/SignUp';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-
+import { createTheme, ThemeProvider } from "@mui/material";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 
 
 function App() {
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Poppins",
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 600,
+      fontWeightBold: 700,
+    },
+  });
 
   return (
 
     <div>
 
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
 
+            <Routes>
+              <Route path="/" element={<LandingMain />}></Route>
+              <Route path="/signin" element={<SignIn />}></Route>
+              <Route path="/signup" element={<SignUp />}></Route>
+            </Routes>
 
-      <BrowserRouter>
-
-        <Routes>
-          <Route path="/" element={<LandingMain />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-        </Routes>
-
-      </BrowserRouter>
+          </BrowserRouter>
+        </ThemeProvider>
+      </StyledEngineProvider>
 
     </div>
 
