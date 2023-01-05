101fce579a7ae2b2b7753735057276c5dc4bf5b3
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2023-01-05 06:25:09 +0530

    commithanges

---
+++ bordimabackend/routes/boardim.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const { getBoardim, addBoardim } = require("../controllers/boardim");
+router.route("/").get(getBoardim).post(addBoardim);
+module.exports = router;
