b7bf81d6f091b458b598039d48636a4754785cda
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2023-01-22 12:15:19 +0530

    added feedback

--- bordimabackend/routes/boardim.js
+++ bordimabackend/routes/boardim.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getBoardim, addBoardim } = require("../controllers/boardim");
+const { addFeedBack } = require("../controllers/feedback");
 router.route("/").get(getBoardim).post(addBoardim);
+router.route("/feedback").post(addFeedBack);
 module.exports = router;
